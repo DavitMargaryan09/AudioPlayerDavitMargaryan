4ca21dc403c273a9cb99eddbff2fe5ac7e8c4b46
DavitMargaryan09 <davit.margaryan4@tumo.org>
2024-05-11 13:28:59 +0400

    data

---
+++ audio_player_Davit_Margaryan2/script.js
@@ -0,0 +1,14 @@
+let data = {
+    title : ["Michael Jackson - Billie jean", 
+    "Eminem – Mockingbird", 
+    "2Pac, Big Syke – All Eyez On Me",
+    "modern-talking - cheri-cheri-lady-mp3"],
+    song : ["music/michael_jackson_this_is_it_13 - Billie Jean.mp3",
+     "music/Eminem – Mockingbird.mp3",
+     "music/2Pac, Big Syke – All Eyez On Me.mp3",
+    "music/modern-talking-cheri-cheri-lady-mp3.mp3"],
+    poster : [
+        "https://downloadwap.com/thumbs3/screensavers/d/new/fcelebs/michael_joseph_jackson-329739.gif",
+        "https://www.what-song.com/_next/image?url=https%3A%2F%2Fi.scdn.co%2Fimage%2Fab67616d00001e02a18291a1854b9b6371ff9663&w=3840&q=75"
+    ]
+}
