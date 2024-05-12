d0953e622740a6f94b6767c9bbc7abe0c8a06646
Davit-Margaryan12 <margaryandavit08@gmail.com>
2024-05-12 18:41:21 +0400

    added changes

--- audio_player_Davit_Margaryan2/script.js
+++ audio_player_Davit_Margaryan2/script.js
@@ -1,14 +1,23 @@
 let data = {
-    title : ["Michael Jackson - Billie jean", 
-    "Eminem – Mockingbird", 
-    "2Pac, Big Syke – All Eyez On Me",
-    "modern-talking - cheri-cheri-lady-mp3"],
-    song : ["music/michael_jackson_this_is_it_13 - Billie Jean.mp3",
-     "music/Eminem – Mockingbird.mp3",
-     "music/2Pac, Big Syke – All Eyez On Me.mp3",
-    "music/modern-talking-cheri-cheri-lady-mp3.mp3"],
+    title : [
+        "Michael Jackson - Billie jean", 
+        "Eminem - Mockingbird", 
+        "Ace Of Base - Happy Nation",
+        "Modern Talking - Cheri Cheri Lady",
+        "PSY - Gangnam Style"
+    ],
+    song : [
+        "music/Michael Jackson - Billie Jean.mp3",
+        "music/Eminem - Mockingbird.mp3",
+        "music/Ace Of Base - Happy Nation.mp3",
+        "music/Modern Talking - Cheri Cheri Lady.mp3",
+        "music/PSY - Gangnam Style.mp3"
+    ],
     poster : [
         "https://downloadwap.com/thumbs3/screensavers/d/new/fcelebs/michael_joseph_jackson-329739.gif",
-        "https://www.what-song.com/_next/image?url=https%3A%2F%2Fi.scdn.co%2Fimage%2Fab67616d00001e02a18291a1854b9b6371ff9663&w=3840&q=75"
+        "https://www.what-song.com/_next/image?url=https%3A%2F%2Fi.scdn.co%2Fimage%2Fab67616d00001e02a18291a1854b9b6371ff9663&w=3840&q=75",
+        "https://upload.wikimedia.org/wikipedia/en/4/4b/Happy_nation.jpg",
+        "https://upload.wikimedia.org/wikipedia/en/d/dd/Cheri_Cheri_Lady.jpg",
+        "https://n1s2.hsmedia.ru/04/3a/7a/043a7a05c46ec85dcce4a861ed1d006f/1200x630_0xac120003_2985818291660227758.png"
     ]
 }
