8d28059d5a355a660b91ed5b9a3fd351c59a3927
Davit-Margaryan12 <margaryandavit08@gmail.com>
2024-05-12 19:25:00 +0400

    changed

--- audio_player_Davit_Margaryan2/script.js
+++ audio_player_Davit_Margaryan2/script.js
@@ -4,20 +4,29 @@ let data = {
         "Eminem - Mockingbird", 
         "Ace Of Base - Happy Nation",
         "Modern Talking - Cheri Cheri Lady",
-        "PSY - Gangnam Style"
+        "PSY - Gangnam Style",
+        "50 Cent - PIMP",
+        "Jennifer Lopez - On The Floor",
+        "XXXtentacion - Moonlight"
     ],
     song : [
         "music/Michael Jackson - Billie Jean.mp3",
         "music/Eminem - Mockingbird.mp3",
         "music/Ace Of Base - Happy Nation.mp3",
         "music/Modern Talking - Cheri Cheri Lady.mp3",
-        "music/PSY - Gangnam Style.mp3"
+        "music/PSY - Gangnam Style.mp3",
+        "music/50 Cent - PIMP.mp3",
+        "music/Jennifer Lopez - On The Floor.mp3",
+        "music/XXXtentacion - Moonlight.mp3"
     ],
     poster : [
         "https://downloadwap.com/thumbs3/screensavers/d/new/fcelebs/michael_joseph_jackson-329739.gif",
         "https://www.what-song.com/_next/image?url=https%3A%2F%2Fi.scdn.co%2Fimage%2Fab67616d00001e02a18291a1854b9b6371ff9663&w=3840&q=75",
         "https://upload.wikimedia.org/wikipedia/en/4/4b/Happy_nation.jpg",
         "https://upload.wikimedia.org/wikipedia/en/d/dd/Cheri_Cheri_Lady.jpg",
-        "https://n1s2.hsmedia.ru/04/3a/7a/043a7a05c46ec85dcce4a861ed1d006f/1200x630_0xac120003_2985818291660227758.png"
+        "https://n1s2.hsmedia.ru/04/3a/7a/043a7a05c46ec85dcce4a861ed1d006f/1200x630_0xac120003_2985818291660227758.png",
+        "https://i.ytimg.com/vi/UDApZhXTpH8/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCC6pO8pbuRJfzRzja7kuE0ZhiD1g",
+        "https://phuketnews.phuketindex.com/wp-content/uploads/2011/03/27.jpg",
+        "https://www.billboard.com/wp-content/uploads/media/XXXTENTACION-MOONLIGHT-2018-billboard-1548.jpg"
     ]
 }
