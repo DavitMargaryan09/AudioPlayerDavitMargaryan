8974336537f47cd3d53c6b5914755d921e2a48ff
Davit-Margaryan12 <margaryandavit08@gmail.com>
2024-05-19 23:15:47 +0400

    finally project

--- audio_player_Davit_Margaryan2/script.js
+++ audio_player_Davit_Margaryan2/script.js
@@ -2,30 +2,30 @@ let data = {
     title : [
         "Michael Jackson - Billie jean", 
         "Eminem - Mockingbird", 
-        "Ace Of Base - Happy Nation",
+        "GTA IV Theme Song",
         "Modern Talking - Cheri Cheri Lady",
         "PSY - Gangnam Style",
-        "50 Cent - PIMP",
+        "Stromae - Alors on danse",
         "Jennifer Lopez - On The Floor",
         "XXXtentacion - Moonlight"
     ],
     song : [
         "music/Michael Jackson - Billie Jean.mp3",
         "music/Eminem - Mockingbird.mp3",
-        "music/Ace Of Base - Happy Nation.mp3",
+        "music/GTA_IV-GTA_IV-world76.spcs.bio.mp3",
         "music/Modern Talking - Cheri Cheri Lady.mp3",
         "music/PSY - Gangnam Style.mp3",
-        "music/50 Cent - PIMP.mp3",
+        "music/Stromae - Alors on danse.mp3",
         "music/Jennifer Lopez - On The Floor.mp3",
         "music/XXXtentacion - Moonlight.mp3"
     ],
     poster : [
         "https://downloadwap.com/thumbs3/screensavers/d/new/fcelebs/michael_joseph_jackson-329739.gif",
-        "https://www.what-song.com/_next/image?url=https%3A%2F%2Fi.scdn.co%2Fimage%2Fab67616d00001e02a18291a1854b9b6371ff9663&w=3840&q=75",
-        "https://upload.wikimedia.org/wikipedia/en/4/4b/Happy_nation.jpg",
-        "https://upload.wikimedia.org/wikipedia/en/d/dd/Cheri_Cheri_Lady.jpg",
+        "https://m.media-amazon.com/images/I/41cLDavIUTL._AC_UF894,1000_QL80_.jpg",
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/12210/capsule_616x353.jpg?t=1695131205",
+        "https://i.makeagif.com/media/10-19-2022/mgq2w1.gif",
         "https://n1s2.hsmedia.ru/04/3a/7a/043a7a05c46ec85dcce4a861ed1d006f/1200x630_0xac120003_2985818291660227758.png",
-        "https://i.ytimg.com/vi/UDApZhXTpH8/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCC6pO8pbuRJfzRzja7kuE0ZhiD1g",
+        "https://i1.sndcdn.com/artworks-N4VzVPwHFSlKDij1-bcOJUg-t500x500.jpg",
         "https://phuketnews.phuketindex.com/wp-content/uploads/2011/03/27.jpg",
         "https://www.billboard.com/wp-content/uploads/media/XXXTENTACION-MOONLIGHT-2018-billboard-1548.jpg"
     ]
@@ -93,7 +93,7 @@ function convertTime(seconds) {
     //     sec
     // }
 
-    totalTime(Math.round(song.duration))
+    totalTime(Math.floor(song.duration))
 }
 
 
@@ -146,6 +146,7 @@ function mute() {
         song.muted = true
         mute.src = "images/volume-mute.png"
     }
+     
 }
 
 function decrease() {
@@ -158,6 +159,13 @@ function decrease() {
 }
 
 function increase() {
+    let mute = document.getElementById("mute")
     song.volume += 0.2
 
+    if(song.muted = false && song.volume <= 0.1) {
+        song.volume = 0.2
+    }
+    if(song.volume > 0.1) {
+        mute.src = "images/volume.png"
+    }
 }
